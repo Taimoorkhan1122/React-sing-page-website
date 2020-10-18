37786e0fb9147efba5f987e38c6aa0266ed4ec86
Taimoor khan <khant8k83@gmail.com>
2020-10-18 19:35:34 +0500

    card component added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from "react";
 import UserIn from "./components/UserIn.js";
 import Navbar from "./components/Navbar.js";
+import Cards from "./components/Cards.js";
 import "./App.css";
-import logo from "./images/sdl_logo_en.svg";
 
 function App() {
+  const cardContent = [
+    "./images/cardcontent/cardcontent-1.png",
+    "./images/cardcontent/cardcontent-2.png",
+    "./images/cardcontent/cardcontent-3.png",
+    "./images/cardcontent/cardcontent-4.png",
+    "./images/cardcontent/cardcontent-5.png",
+    "./images/cardcontent/cardcontent-6.png",
+  ];
   return (
     <div className="App">
       {/* Start of Header */}
       <header className="App-header">
         <div className="header-container">
           <a href="#home">
-            <img src={logo} alt="logo-icon" />
+            <img src={"./images/sdl_logo_en.svg"} alt="logo-icon" />
           </a>
           <Navbar />
           <UserIn />
         </div>
       </header>
 
-      <main className="App-main">this is main</main>
-      <code>hello</code>
+      <main className="App-main">
+        <Cards paths={cardContent} />
+      </main>
+
       <footer className="App-footer">This is footer</footer>
     </div>
   );
---
+++ src/components/Cards.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./card.css";
+
+function Cards({ paths }) {
+  return (
+    <div className="card-img-container">
+      {paths.map((imgUrl, index) => {
+        return (
+          <div className="card-img" key={index}>
+            <img src={imgUrl} key={index + 1} alt={`card ${index}`} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Cards;
